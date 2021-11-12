897e59410599dca089547f5f8c22dc5b4badbd71
Mohamed amine Baioumy <mbaioumy@e2r5p3.1337.ma>
2021-11-12 13:59:04 +0100

    more bug fixes

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,24 +6,13 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 18:54:34 by mbaioumy          #+#    #+#             */
-/*   Updated: 2021/11/11 12:52:44 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2021/11/12 13:35:40 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
-void	ft_bzero(void *s, size_t n)
-{
-    int     i;
-	char    *str = (char *)s;
-
-	i = 0;
-	while (i < (int)n)
-	{
-		str[i] = '\0';
-		i++;
-    }
-}
+void	ft_bzero(void *s, size_t n);
 
 void    *ft_calloc (size_t count, size_t size)
 {
@@ -32,10 +21,17 @@ void    *ft_calloc (size_t count, size_t size)
 
     len = count * size;
     ptr = malloc(len * sizeof(char));
+    if (!ptr)
+        return NULL;
     ft_bzero(ptr, len);
     return ptr;
 }
-
+/*int main(void)
+{
+    char *ptr;
+    ptr = calloc(4, 20);
+    printf("%i", ptr[2]);
+}*/
  /*int    main()
  {
      char *str;
--- ft_substr.c
+++ ft_substr.c
@@ -6,46 +6,53 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 13:42:57 by mbaioumy          #+#    #+#             */
-/*   Updated: 2021/11/08 14:25:19 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2021/11/12 13:37:16 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+unsigned int ft_strlen(char *str);
+char	*ft_strdup(char *src);
+void    *ft_calloc (size_t count, size_t size);
 
 char    *ft_substr(char const *s, unsigned int start, size_t len)
 {
     unsigned int i;
     unsigned int j;
     char    *str;
+    char *s1;
 
-    str = (char *)malloc((len * sizeof(char)) + 1);
+    if (s == NULL)
+        return NULL;
+    s1 = (char *)s;
     i = start;
     j = 0;
-    if(start == (unsigned int)len)
-        return str;
-    if (str == NULL)
+    
+    if (len == 0 || start > strlen(s1))
+        return ft_strdup("");
+    if (len > ft_strlen(s1 + start))
+        str = (char *)ft_calloc((ft_strlen(s1 + start) + 1), sizeof(char));
+    if (len <= ft_strlen(s1 + start))
+        str = (char *)ft_calloc(len + 1, sizeof(char));
+    if (!str)
         return NULL;
-    while (j < (unsigned int)len)
+    while (s1[i] && (size_t)j < len)
     {
-        str[j] = s[i];
+       str[j] = s1[i];
         i++;
         j++;
     }
-    str[j] = '\0';
     return str;
 }
 
 /*int main()
 {
-    char *str = "Amine";
-    char *str2 = "i2h129hUIGq";
-    char *str3 = "ojqoHQHQBNbiwqb2441-";
-    char *str4 = "eoqrjorq";
-    printf("%s\n", ft_substr(str, 1, 4));
-    printf("%s\n", ft_substr(str2, 2, 2));
-    printf("%s\n", ft_substr(str3, 3, 4));
-    printf("%s\n", ft_substr(str4, 6, 1));
+    char *str = strdup("0123456789");
+	char *s = ft_substr(str, 9, 10);
+    printf("%s\n", s);
     return 0;
 }*/
